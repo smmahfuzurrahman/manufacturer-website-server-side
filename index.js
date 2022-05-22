f51a0f167e422d7f93776372ef07bb1578fb9d97
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-22 23:31:32 +0600

    products data load

--- index.js
+++ index.js
@@ -4,16 +4,34 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// middle ware
+// Middle Ware
 app.use(cors());
 app.use(express.json());
 const jwt = require('jsonwebtoken');
 
+// Backend Connected Code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcqpd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log("Connected");
+
 async function run() {
     try {
+        await client.connect();
+        console.log("Data Connected");
+
+        const productsCollection = client.db('computer_village').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const productInfo = await productsCollection.findOne(query);
+            res.send(productInfo);
+        })
 
     }
     finally {
