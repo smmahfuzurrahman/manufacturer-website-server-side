3f2db796886e1cf2bb2adef206ccb509275aee88
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-24 12:48:46 +0600

    different email loaded

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Backend Connected Code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcqpd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Jwt Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Un Authorized Access" });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 
 async function run() {
     try {
@@ -20,6 +38,19 @@ async function run() {
 
         const productsCollection = client.db('computer_village').collection('products');
         const myOrderCollection = client.db('computer_village').collection('myOrder');
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
         // GET ALL PRODUCT IN products server
         app.get('/products', async (req, res) => {
             const query = {};
@@ -42,7 +73,7 @@ async function run() {
         })
         // Get single product take database show MyOrder route
         app.get('/myorder', async (req, res) => {
-            const email = req.query.userEmail;
+            const email = req.query.email;
             const query = { email: email }
             const cursor = myOrderCollection.find(query);
             const items = await cursor.toArray();
@@ -50,8 +81,8 @@ async function run() {
         });
         // Get Singel My Order /myorder/:id
         app.get('/myorder/:id', async (req, res) => {
-            const email = req.query.userEmail;
-            const query = { email: email }
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const productInfo = await myOrderCollection.findOne(query);
             res.send(productInfo);
         })
