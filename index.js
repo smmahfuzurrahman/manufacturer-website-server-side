e87b0d87a4824db004be1c56cbd3b1accee4bb50
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-06-02 13:08:24 +0600

    update app.use(cors)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middle Ware
-app.use(cors());
+app.use(cors({ origin: 'https://computer-villages.web.app' }));
 app.use(express.json());
 const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -66,7 +66,7 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
         })
 
