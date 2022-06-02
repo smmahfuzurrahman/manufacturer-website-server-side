94614624af5eff7a37b44d9787cb7b7c245224f2
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-06-02 20:10:08 +0600

    Heroku Link Upated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middle Ware
-app.use(cors({ origin: 'https://computer-villages.web.app' }));
+app.use(cors());
 app.use(express.json());
 const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
